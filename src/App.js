4c5a8973fa41d4677e74e3e56d7e05ae2cc95133
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-06-25 14:51:02 +0200

    begin state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React,  { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchPostcode: "",
+      searchStreet: "",
+      searchNumber: "",
+      searchAddition: "",
+      searchAddressState: "NOT_SEND", // "SEND", "RECEIVED", "FAILED"
+      foundAddressesList: [],
+      selectedAddress: null,
+      getWaterlabelState: "NOT_SEND", // "SEND", "RECEIVED" , "FAILED"
+      serverWaterlabels: [],
+      editedWaterlabel: null,
+      email: "",
+      waterlabelSaved: "NOT_SEND", // "SEND", "RECEIVED" , "FAILED"
+      guiShowVideo: false,
+      guiShowEmail: false,
+      guidShowSuccesSave: false,
+      guiEditLabel: false,
+      guiLabelTab: "DAK", // "TUIN", "OVERIG"
+      guiInfoTab: "PERSONAL", // "CALCULATION", "WHY"
+    };
+  };
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
