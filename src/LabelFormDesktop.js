a78f619010cde2fc270b065ac4958c19d7ef8da8
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-07-16 16:30:33 +0200

    make svg react compatible

--- src/LabelFormDesktop.js
+++ src/LabelFormDesktop.js
@@ -77,7 +77,7 @@ export default function LabelFormDesktop (props) {
             }
           }}
         >
-          Extra Details
+          <span>Extra Details</span>
         </button>
         
       
