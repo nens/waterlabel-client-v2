dd36d000afd1a159b4c9134ed8bf4a59eda4ff34
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-07-17 14:43:17 +0200

    remove commented out code

--- src/SearchAddressForm.js
+++ src/SearchAddressForm.js
@@ -37,7 +37,6 @@ function SearchAddressForm (props) {
   } = props;
 
   return (
-    // {/*_______________________________________ SEARCH ADDDRESS FORM */}
     <div
       className={
         foundAddressesList.length === 0 &&
@@ -49,214 +48,193 @@ function SearchAddressForm (props) {
     >
       <div
         className="SearchAddressFormContainer"
-        // className={
-        //   foundAddressesList.length === 0 &&
-        //   assetTypeFetchState === "RECEIVED" ? 
-        //   "SearchAddressFormContainer" 
-        //   : 
-        //   "SearchAddressFormContainer HideSearchAddressForm"
-        // }
       >
       <div
-      className={
-        // class was moved to div higher
-        // foundAddressesList.length === 0 &&
-        // assetTypeFetchState === "RECEIVED" ? 
-        "SearchAddressForm" 
-        // : 
-        // "SearchAddressForm HideSearchAddressForm"
-      }
-    >
-      <div
-        className={searchAddressState === "SEND" ? 
-          "SpinnerContainer Visible" 
-          : 
-          "SpinnerContainer Invisible" 
-        }
+        className="SearchAddressForm" 
       >
-        <LoadingIcon
-          singleColor={"white"}
-          size={50}
-        />
-        <div>Zoeken adressen</div>
-      </div>
-
-      <form
-      // style={
-      //   foundAddressesList.length === 0 &&
-      //   assetTypeFetchState === "RECEIVED" ? 
-      //   {} : {display: "none"} 
-      // }
-      >
-        <h1>Waterlabel.net</h1>
-        <span>Wat doe ik tegen wateroverlast?</span>
-      
-        <button
-          className="YoutubeButton"
-          onClick={e=>{
-            e.preventDefault();
-            setGuiShowVideo(true);
-          }}
-        >
-          <div>Kijk hier hoe het werkt</div>
-          <i>
-            <img src={playButton} alt="Play-Video" />
-          </i>
-        </button>
-
-        <legend>
-          Zoek hier naar een woning
-        </legend>
-
-        <div className="InputBlock">
         <div
-          className="Row"
-          style={
-            searchOnCityStreet===true ? {} : {display: "none"} 
+          className={searchAddressState === "SEND" ? 
+            "SpinnerContainer Visible" 
+            : 
+            "SpinnerContainer Invisible" 
           }
         >
-          <label 
-            className="LabelLeft" 
-            htmlFor="searchCity"
-          >
-            Stad
-          </label>
-          <input
-            className="InputWholeRow"
-            id="searchCity"
-            value={searchCity}
-            onChange={e=>setSearchCity( e.target.value)}
-            placeholder="Bijv. Amsterdam"
+          <LoadingIcon
+            singleColor={"white"}
+            size={50}
           />
+          <div>Zoeken adressen</div>
         </div>
+
+        <form>
+          <h1>Waterlabel.net</h1>
+          <span>Wat doe ik tegen wateroverlast?</span>
         
-        
+          <button
+            className="YoutubeButton"
+            onClick={e=>{
+              e.preventDefault();
+              setGuiShowVideo(true);
+            }}
+          >
+            <div>Kijk hier hoe het werkt</div>
+            <i>
+              <img src={playButton} alt="Play-Video" />
+            </i>
+          </button>
+
+          <legend>
+            Zoek hier naar een woning
+          </legend>
+
+          <div className="InputBlock">
           <div
             className="Row"
             style={
-              searchOnCityStreet===false ? {} : {display: "none"} 
+              searchOnCityStreet===true ? {} : {display: "none"} 
             }
           >
             <label 
               className="LabelLeft" 
-              htmlFor="searchPostcode"
+              htmlFor="searchCity"
             >
-              Postcode
+              Stad
             </label>
             <input
               className="InputWholeRow"
-              id="searchPostcode"
-              value={searchPostcode}
-              onChange={e=>setSearchPostcode(e.target.value)}
-              placeholder="1234 AB"
+              id="searchCity"
+              value={searchCity}
+              onChange={e=>setSearchCity( e.target.value)}
+              placeholder="Bijv. Amsterdam"
             />
           </div>
           
-          <div
-            className="Row"
-            style={
-              searchOnCityStreet===true ? {} : {display: "none"} 
-            }
-          >
-            <label 
-              className="LabelLeft" 
-              htmlFor="searchStreet"
+          
+            <div
+              className="Row"
+              style={
+                searchOnCityStreet===false ? {} : {display: "none"} 
+              }
             >
-            Straat
-            </label>
-            <input
-              className="InputWholeRow"
-              id="searchStreet"
-              value={searchStreet}
-              onChange={e=>setSearchStreet(e.target.value)}
-            />
-          </div>
+              <label 
+                className="LabelLeft" 
+                htmlFor="searchPostcode"
+              >
+                Postcode
+              </label>
+              <input
+                className="InputWholeRow"
+                id="searchPostcode"
+                value={searchPostcode}
+                onChange={e=>setSearchPostcode(e.target.value)}
+                placeholder="1234 AB"
+              />
+            </div>
+            
+            <div
+              className="Row"
+              style={
+                searchOnCityStreet===true ? {} : {display: "none"} 
+              }
+            >
+              <label 
+                className="LabelLeft" 
+                htmlFor="searchStreet"
+              >
+              Straat
+              </label>
+              <input
+                className="InputWholeRow"
+                id="searchStreet"
+                value={searchStreet}
+                onChange={e=>setSearchStreet(e.target.value)}
+              />
+            </div>
 
-          <div
-            className="Row" 
-          >
-            <label 
-              className="LabelLeft" 
-              htmlFor="searchNumber"
+            <div
+              className="Row" 
             >
-              Huisnr.
-            </label>
-            <input
-              id="searchNumber"
-              value={searchNumber}
-              onChange={e=>setSearchNumber(e.target.value)}
-            />
-            <label
-              className="LabelRight" 
-              htmlFor="searchAddition">
-              Toev.
-            </label>
-            <input
-              id="searchAddition"
-              value={searchAddition}
-              onChange={e=>setSearchAddition(e.target.value)}
-            />
+              <label 
+                className="LabelLeft" 
+                htmlFor="searchNumber"
+              >
+                Huisnr.
+              </label>
+              <input
+                id="searchNumber"
+                value={searchNumber}
+                onChange={e=>setSearchNumber(e.target.value)}
+              />
+              <label
+                className="LabelRight" 
+                htmlFor="searchAddition">
+                Toev.
+              </label>
+              <input
+                id="searchAddition"
+                value={searchAddition}
+                onChange={e=>setSearchAddition(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
-        <div>
-          <button
-            className="ButtonToggleStreetPostcode"
-            style={
-              searchOnCityStreet===false ? {} : {display: "none"} 
-            }
-            onClick={ e => {
-              e.preventDefault();
-              setSearchOnCityStreet(true)
-            }}
-          >
-            Zoek op straat
-          </button>
-          <button
-            className="ButtonToggleStreetPostcode"
-            style={
-              searchOnCityStreet===true ? {} : {display: "none"} 
-            }
-            onClick={ e => {
-              e.preventDefault();
-              setSearchOnCityStreet(false)
-            }}
-          >
-            Zoek op postcode
-          </button>
-        </div>
-        {/* <div> */}
-          <button
-            className="ButtonSearch StandardButton"
-            onClick={ e => {
-              e.preventDefault();
-              fetchBuildings();
-            }}
-          >
-            Zoek
-          </button>
-          <div
-            className="ErrorText"
-            style={foundAddressesList.length === 0 && searchAddressState === "RECEIVED" ? {} : {display: "none"}}
-          >
-            <div>Onbekend adres</div>
-            <div>Probeert u het nogmaals </div>
+          <div>
+            <button
+              className="ButtonToggleStreetPostcode"
+              style={
+                searchOnCityStreet===false ? {} : {display: "none"} 
+              }
+              onClick={ e => {
+                e.preventDefault();
+                setSearchOnCityStreet(true)
+              }}
+            >
+              Zoek op straat
+            </button>
+            <button
+              className="ButtonToggleStreetPostcode"
+              style={
+                searchOnCityStreet===true ? {} : {display: "none"} 
+              }
+              onClick={ e => {
+                e.preventDefault();
+                setSearchOnCityStreet(false)
+              }}
+            >
+              Zoek op postcode
+            </button>
           </div>
-        {/* </div> */}
-        <div>
-          <a 
-            href="https://waterlabel-v1.lizard.net/"
-            className="LinkOldSite"
-            // target = blank is probably a bad idea
-            // target="_blank"
-          >
-            Klik hier voor de oude waterlabel site
-          </a>
-        </div>
-      </form>
-    </div>
-    <div
-        className="Logos"
-      >
+          {/* <div> */}
+            <button
+              className="ButtonSearch StandardButton"
+              onClick={ e => {
+                e.preventDefault();
+                fetchBuildings();
+              }}
+            >
+              Zoek
+            </button>
+            <div
+              className="ErrorText"
+              style={foundAddressesList.length === 0 && searchAddressState === "RECEIVED" ? {} : {display: "none"}}
+            >
+              <div>Onbekend adres</div>
+              <div>Probeert u het nogmaals </div>
+            </div>
+          <div>
+            <a 
+              href="https://waterlabel-v1.lizard.net/"
+              className="LinkOldSite"
+              // target = blank is probably a bad idea
+              // target="_blank"
+            >
+              Klik hier voor de oude waterlabel site
+            </a>
+          </div>
+        </form>
+      </div>
+      <div
+          className="Logos"
+        >
         {/* van deWaag bestaat geen website ? */}
         <img
           alt="Logo de Waag"
