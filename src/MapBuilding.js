ea3f716524f799cd89c15e28dd0f1edbb99e50e3
GeOdin <madeleine.vanwinkel@gmail.com>
2020-01-20 11:54:05 +0100

    Remove more comments.

--- src/MapBuilding.js
+++ src/MapBuilding.js
@@ -4,7 +4,8 @@ import { Map, TileLayer, Marker, FeatureGroup, Polygon } from "react-leaflet";
 import './MapBuilding.css';
 
 function getPolygonBounds (geoJSON) {
-	// Note: does not work for polygons crossing the datetime border.
+	// Note: does not work for polygons crossing the datetime border
+	// (where xMax is less than xMin).
 	const coordinates = geoJSON.coordinates[0];
 	let xMin = coordinates[0][0];
 	let xMax = coordinates[0][0];
@@ -16,11 +17,11 @@ function getPolygonBounds (geoJSON) {
 		if (coordinates[i][1] < yMin) {yMin = coordinates[i][1]};
 		if (coordinates[i][1] > yMax) {yMax = coordinates[i][1]};
 	}
-	// console.log(xMin, xMax, yMin, yMax); // 5.558407963674824, 5.55882798548451, 52.02886037368467, 52.02908930582589
-	// 5.558407963674824,5.55882798548451,52.02886037368467,52.02908930582589
-	// 5.558407963674824,52.02886037368467,5.55882798548451,52.02908930582589
-	// 52.02886037368467,5.558407963674824,52.02908930582589,5.55882798548451
-	// 52.02886037368467,52.02908930582589,5.558407963674824,5.55882798548451
+	// console.log(xMin, xMax, yMin, yMax);
+	// 5.558407963674824,5.55882798548451,52.02886037368467,52.02908930582589 // do we want this for the in_bbox parameter for the api?
+	// 5.558407963674824,52.02886037368467,5.55882798548451,52.02908930582589 // do we want this for the in_bbox parameter for the api?
+	// 52.02886037368467,5.558407963674824,52.02908930582589,5.55882798548451 // do we want this for the in_bbox parameter for the api?
+	// 52.02886037368467,52.02908930582589,5.558407963674824,5.55882798548451 // do we want this for the in_bbox parameter for the api?
 	return {xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax};
 }
 
@@ -31,42 +32,18 @@ function getPolygonCenter (polygonBounds) {
 }
 
 export default function MapBuilding (props) {
-    const { buildingGeoJSON, selectedAddress, waterLabelColor } = props;//, buildingGeoJSON//, selectedAddress,
+    const { buildingGeoJSON, selectedAddress, waterLabelColor } = props;
     const polygonBounds = getPolygonBounds(buildingGeoJSON);
     const polygonCenter = getPolygonCenter(polygonBounds);
-    const zoomLevel = 19;//19 is the most zoomed in you still get a basemap
-    let position = [polygonCenter.yMean, polygonCenter.xMean];//polygon.getBounds().getCenter();//[52.02891795362399, 5.558407963674824]; //[52.092802, 5.1137246];
-    // if (buildingGeoJSON===undefined || buildingGeoJSON===null) {
-    // 	return (null);
-    // }
+    const zoomLevel = 19; // zoomlevel 19 is the most zoomed in you still get a basemap
+    let position = [polygonCenter.yMean, polygonCenter.xMean];
     let latlngs = [];
-    // let latlngs2 = [ //Achterstraatje Veenendaal //3901BA nummer 16
- //      [52.02891795362399, 5.558407963674824],
- //      [52.028895006882905, 5.558556125263742],
- //      [52.028894638584, 5.558555978139519],
- //      [52.028882464748605, 5.558638842742482],
- //      [52.028883075597996, 5.558639078224903],
- //      [52.02886037368467, 5.558791728506017],
- //      [52.02886861390884, 5.558811504331883],
- //      [52.02891064530849, 5.55882798548451],
- //      [52.028992451721535, 5.558707166953551],
- //      [52.02904320489045, 5.558616595942235],
- //      [52.02904282819822, 5.55861604078445],
- //      [52.02908930582589, 5.558529664351089],
- //      [52.029016624665395, 5.558470124059828],
- //      [52.02891795362399, 5.558407963674824]
-    // ];
+
     if (buildingGeoJSON) {
         for (var i = 0; i < buildingGeoJSON.coordinates[0].length; i++) {
-            // if (i) {return ;}
-            // if (i !== 0) {
-            // console.log(buildingGeoJSON.coordinates[0][i]);
             let lat = buildingGeoJSON.coordinates[0][i][0];
-            // console.log(lat);
             let lng = buildingGeoJSON.coordinates[0][i][1];
-            // console.log(lng);
-            latlngs.push([lng, lat]); // deze volgorde klopt voor Polygon van leaflet
-            // }
+            latlngs.push([lng, lat]); // this order is right for Polygon of react-leaflet
         }
     } else {
 	    latlngs = [ //Achterstraatje Veenendaal //3901BA nummer 16
@@ -86,58 +63,12 @@ export default function MapBuilding (props) {
           [52.02891795362399, 5.558407963674824]
         ];
     }
-    // console.log(selectedAddress);
-    // let key = new Date().getTime().toString();
-    // console.log(key);
-    // let buildingGeoJSON2 = {
- //        type: "Polygon",
- //        coordinates: [
-    // 		// [4.721374511718751, 52.670761340824384], // Heerhugowaard inverted voor GeoJSON
-    // 		// [4.779396057128906, 52.657435216573454],
-    // 		// [4.756393432617188, 52.649937485958795],
-    // 		// [4.699745178222657, 52.65181203915606]
-    // 		// { lat: 4.721374511718751, lng: 52.670761340824384 }, // Heerhugowaard inverted voor GeoJSON
-    // 		// { lat: 4.779396057128906, lng: 52.657435216573454 },
-    // 		// { lat: 4.756393432617188, lng: 52.649937485958795 },
-    // 		// { lat: 4.699745178222657, lng: 52.65181203915606 }
-    // 		// { lat: 52.670761340824384, lng: 4.721374511718751 }, // Heerhugowaard
-    // 		// { lat: 52.657435216573454, lng: 4.779396057128906 },
-    // 		// { lat: 52.649937485958795, lng: 4.756393432617188 },
-    // 		// { lat: 52.65181203915606, lng: 4.699745178222657 }
- //    	],
- //        // coordinates: [
- //        // 	{lat: 52.02891795362399, lng: 5.558407963674824},//, 43.553980556316674], // Veenendaal
- //        // 	{lat: 52.028895006882905, lng: 5.558556125263742},//, 43.554011701606214],
- //        // 	{lat: 52.028894638584, lng: 5.558555978139519},//, 43.5540123321116],
- //        // 	{lat: 52.028882464748605, lng: 5.558638842742482},// 43.554028634913266],
- //        // 	// [{lat: 52.02891795362399, lng: 5.558407963674824}]//, 43.554028634913266]
- //        // ],
- //        // coordinates: [
- //        //   [5.558407963674824, 52.02891795362399, 43.553980556316674],
- //        //   [5.558556125263742, 52.028895006882905, 43.554011701606214],
- //        //   [5.558555978139519, 52.028894638584, 43.5540123321116],
- //        //   [5.558638842742482, 52.028882464748605, 43.554028634913266],
- //        //   [5.558639078224903, 52.028883075597996, 43.55402759555727],
- //        //   [5.558791728506017, 52.02886037368467, 43.55405809171498],
- //        //   [5.558811504331883, 52.02886861390884, 43.55404319893569],
- //        //   [5.55882798548451, 52.02891064530849, 43.55397152528167],
- //        //   [5.558707166953551, 52.028992451721535, 43.5538397943601],
- //        //   [5.558616595942235, 52.02904320489045, 43.55375886056572],
- //        //   [5.55861604078445, 52.02904282819822, 43.55375952459872],
- //        //   [5.558529664351089, 52.02908930582589, 43.55368557944894],
- //        //   [5.558470124059828, 52.029016624665395, 43.5538111012429],
- //        //   // [5.558407963674824, 52.02891795362399, 43.553980556316674], // filter out point if last is first https://leafletjs.com/reference-1.5.0.html#polygon
- //        // ],
- //  	};
-    // console.log(buildingGeoJSON2.coordinates);
-    // let buildingCode = "";
-    // if (selectedAddress) {buildingCode = selectedAddress.building; } //0345100002008052
-    // console.log(buildingCode);
+
     const map = (
         <Map
           center={position}
           zoom={zoomLevel}
-          zoomControl={false} // removes + / - zoomcontrol on the map
+          zoomControl={false} // removes +/- zoomcontrol on the map
           doubleClickZoom={false}
           closePopupOnClick={false}
           dragging={false}
@@ -156,21 +87,14 @@ export default function MapBuilding (props) {
             <Polygon
               positions={latlngs}
               color={waterLabelColor}
-              height="50"
-              width="50"
-              //style={{color: "blue"}}// height: "50px", width: "50px",  //margin: "0 auto", height: "100%", width: "100%", 
             />
           </FeatureGroup>
         </Map>
     );
-    // map.zoomControl.remove();
-    // console.log(map.getBounds());
-    // var polygon = L.polygon(buildingGeoJSON.coordinates, {color: 'red'}).addTo(map);
+
     return (
         <div className="MapContainer">
             {map}
         </div>
     );
-        // {selectedAddress ? map : null}
-    //		{map}
 }
