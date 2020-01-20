4f52f6ce3272d07525b20e43a519ce1031f6c6e4
GeOdin <madeleine.vanwinkel@gmail.com>
2020-01-20 15:38:51 +0100

    Refactor polygon functions out of MapBuilding.

--- src/MapBuilding.js
+++ src/MapBuilding.js
@@ -1,30 +1,8 @@
 import React from 'react';
 import { Map, TileLayer, Marker, FeatureGroup, Polygon } from "react-leaflet";
 
+import {getPolygonBounds, getPolygonCenter} from './utils/polygonFunctions';
 import './MapBuilding.css';
-
-function getPolygonBounds (geoJSON) {
-  // Note: does not work for polygons crossing the datetime border
-  // (where xMax is less than xMin).
-  const coordinates = geoJSON.coordinates[0];
-  let xMin = coordinates[0][0];
-  let xMax = coordinates[0][0];
-  let yMin = coordinates[0][1];
-  let yMax = coordinates[0][1];
-  for (var i = 0; i < coordinates.length; i++) {
-    if(coordinates[i][0] < xMin) {xMin = coordinates[i][0]};
-    if(coordinates[i][0] > xMax) {xMax = coordinates[i][0]};
-    if (coordinates[i][1] < yMin) {yMin = coordinates[i][1]};
-    if (coordinates[i][1] > yMax) {yMax = coordinates[i][1]};
-  }
-  return {xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax};
-}
-
-function getPolygonCenter (polygonBounds) {
-  let xMean = (polygonBounds.xMin + polygonBounds.xMax) / 2;
-  let yMean = (polygonBounds.yMin + polygonBounds.yMax) / 2;
-  return {xMean: xMean, yMean: yMean};
-}
 
 export default function MapBuilding (props) {
   const { buildingGeoJSON, selectedAddress, waterLabelColor } = props;
---
+++ src/utils/polygonFunctions.js
@@ -0,0 +1,22 @@
+export function getPolygonBounds (geoJSON) {
+  // Note: does not work for polygons crossing the datetime border
+  // (where xMax is less than xMin).
+  const coordinates = geoJSON.coordinates[0];
+  let xMin = coordinates[0][0];
+  let xMax = coordinates[0][0];
+  let yMin = coordinates[0][1];
+  let yMax = coordinates[0][1];
+  for (var i = 0; i < coordinates.length; i++) {
+    if(coordinates[i][0] < xMin) {xMin = coordinates[i][0]};
+    if(coordinates[i][0] > xMax) {xMax = coordinates[i][0]};
+    if (coordinates[i][1] < yMin) {yMin = coordinates[i][1]};
+    if (coordinates[i][1] > yMax) {yMax = coordinates[i][1]};
+  }
+  return {xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax};
+}
+
+export function getPolygonCenter (polygonBounds) {
+  let xMean = (polygonBounds.xMin + polygonBounds.xMax) / 2;
+  let yMean = (polygonBounds.yMin + polygonBounds.yMax) / 2;
+  return {xMean: xMean, yMean: yMean};
+}
